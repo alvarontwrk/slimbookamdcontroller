765420637da0e6d2db20751c583b596f6bb1f6db
Alvaro GJ <garciajaenalvaro@gmail.com>
2023-08-03 20:37:28 +0200

    rm

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,34 @@ fi
 
 xbps-install -y RyzenAdj
 
+echo "Deleting previous installation"
+rm -rf /usr/share/slimbookamdcontroller
+rm /usr/share/applications/slimbookamdcontroller.desktop
+rm -rf /usr/share/icons/hicolor/1024x1024/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/128x128/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/16x16/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/192x192/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/20x20/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/22x22/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/24x24/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/256x256/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/28x28/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/32x32/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/36x36/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/40x40/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/480x480/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/48x48/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/512x512/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/64x64/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/72x72/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/8x8/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/96x96/apps/slimbookamdcontroller.png
+rm -rf /usr/share/icons/hicolor/scalable/apps/slimbookamdcontroller.svg
+rm -rf /etc/udev/rules.d/81.power.rules
+rm -rf /usr/bin/slimbookamdcontroller
+rm -rf /usr/bin/slimbookamdcontroller-pkexec
+rm -rf /usr/share/polkit-1/actions/com.ubuntu.pkexec.slimbookamdcontroller.policy
+
 mkdir -p /usr/share/slimbookamdcontroller/src
 
 echo "Copying contents..."
